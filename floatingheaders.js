dc9a697c0c3172cc31697303af1aa5f509fd7b50
mattwelson <matthew.welson@gmail.com>
2015-04-28 22:45:50 +1200

    Not working with translate

---
+++ floatingheaders.js
@@ -0,0 +1,26 @@
+﻿$(window).scroll(function () {
+    var wScroll = $(this).scrollTop();
+
+    var onscreen = null,
+        offsetFromTop;
+
+    $('.fh-container').each(function () {
+        var $this = $(this);
+        if ($this.offset().top < wScroll && $this.offset().top + $this.height() > wScroll) {
+            onscreen = this;
+            offsetFromTop = $this.offset().top;
+        }
+    });
+
+    if (onscreen) {
+        var $header = $('.fh-header', onscreen);
+
+        if ($header.length) {
+            $header.css({
+                'transform': 'translate(0px, ' + wScroll - offsetFromTop + 'px)'
+            });
+
+            console.log(wScroll - offsetFromTop);
+        }
+    }
+});
